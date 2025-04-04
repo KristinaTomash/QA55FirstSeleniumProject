c02f2afb123e7324ba348f8b220fb159ace8c6f0
KristinaTomash <kristitomsh@gmail.com>
2025-04-04 13:57:13 +0300

    Add

--- src/test/java/com/ait/qa55/FirstSeleniumTest.java
+++ src/test/java/com/ait/qa55/FirstSeleniumTest.java
@@ -6,6 +6,8 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
 public class FirstSeleniumTest {
     WebDriver driver;
 
@@ -13,9 +15,17 @@ public class FirstSeleniumTest {
     public void setUp(){
         driver= new ChromeDriver();
         //driver.get("https://www.google.com");//without history
-       driver.navigate().to("https://seferisrael.co.il");//with history
+        //maximize browser to window
+        driver.manage().window().maximize();
+
+           driver.navigate().to("https://seferisrael.co.il");//with history
+
+        //wait for all elements on the site to load before starting test
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
         driver.navigate().to("https://www.google.com");
         driver.navigate().back();
+        driver.navigate().refresh();
 
     }
     @Test
@@ -26,7 +36,9 @@ public class FirstSeleniumTest {
 
     @AfterMethod(enabled = false)
     public void tearDown(){
-        driver.quit();
+
+        //driver.quit(); // all tabs and browser
+        driver.close();// only one tab(if tan only one->close browser)
     }
 
 
